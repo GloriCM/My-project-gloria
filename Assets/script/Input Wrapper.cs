29dbdf7c2517cc79549781a147d1506a516317d3
innmajo <maochebarrera@gmail.com>
2024-08-26 14:57:46 -0500

    Commit Inicial

---
+++ Assets/script/Input Wrapper.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputWrapper : MonoBehaviour
+{
+    [SerializeField] 
+    private float speed;
+    private PlayerInput inputs;
+    private InputAction jumpAction;
+    private InputAction movementAction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      inputs = GetComponent<PlayerInput>();  
+      jumpAction = inputs.actions["Jump"]; 
+      movementAction = inputs.actions["Walk"]; 
+      Debug.Log(inputs); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(movementAction.ReadValue<float>());
+    }
+}
